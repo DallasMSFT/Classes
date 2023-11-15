1c99e21dc00afb2051616e957657c46456d4f736
Dallas Cecil <DallasCecil@microsoft.com>
2023-11-15 15:18:39 -0500

    Added TypeData

--- Stuff.ps1
+++ Stuff.ps1
@@ -1,15 +1,24 @@
 $myObject = [PSCustomObject]@{
-    Make  = 'Ford'
-    Model = 'F150'
-    Color = 'Blue'
-    Year  = 2050
+    PSTypeName = 'dkc.Vehicle'
+    Make       = 'Ford'
+    Model      = 'F150'
+    Color      = 'Blue'
+    Year       = 2050
 }
-$myCar = [Vehicle]@{
-    Make  = 'Chevy'
-    Model = 'Van'
-    Color = 'Green'
-    Year  = 2000
+# $myCar = [Vehicle]@{
+#     Make  = 'Chevy'
+#     Model = 'Van'
+#     Color = 'Green'
+#     Year  = 2000
+# }
+
+
+$TypeData = @{
+    TypeName                  = 'dkc.Vehicle'
+    DefaultDisplayPropertySet = 'Make', 'Model'
 }
+Update-TypeData @TypeData -Force -ErrorAction SilentlyContinue
+
 
 $myObject
 $myCar
